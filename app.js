c848ecbfcf2cf62999d445297586bdabbac4bc95
GemelliAngelo <gemeangelo27@gmail.com>
2024-11-19 14:24:26 +0100

    add posts and titles

--- app.js
+++ app.js
@@ -3,7 +3,43 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Server del mio Blog");
+});
+
+app.get("/bacheca", (req, res) => {
+  const posts = [
+    (post1 = {
+      title: "Ciambellone",
+      content: "Contenuto",
+      image: "",
+      tags: [""],
+    }),
+    (post2 = {
+      title: "Cracker alla barbabietola ",
+      content: "Contenuto",
+      image: "",
+      tags: [""],
+    }),
+    (post3 = {
+      title: "Pane fritto dolce",
+      content: "Contenuto",
+      image: "",
+      tags: [""],
+    }),
+    (post4 = {
+      title: "Pasta alla barbabietola",
+      content: "Contenuto",
+      image: "",
+      tags: [""],
+    }),
+    (post5 = {
+      title: "Torta paesana",
+      content: "Contenuto",
+      image: "",
+      tags: [""],
+    }),
+  ];
+  res.json(posts);
 });
 
 app.listen(port, () => {
